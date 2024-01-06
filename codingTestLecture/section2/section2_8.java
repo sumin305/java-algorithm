d30886eb90297025be68aabe6c342cf3ddd19747
gogotnals <gogotnals@naver.com>
2024-01-06 17:42:52 +0900

    section2-8 등수구하기

--- codingTestLecture/section2/section2_8.java
+++ codingTestLecture/section2/section2_8.java
@@ -1,4 +1,22 @@
 package codingTestLecture.section2;
-
+import java.lang.reflect.Array;
+import java.util.*;
 public class section2_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+
+        for (int i = 0; i < N; i ++) {
+            int n = sc.nextInt();
+            arr[i] = n;
+            temp.add(n);
+        }
+
+        Collections.sort(temp, Collections.reverseOrder());
+        for (int i = 0; i < N; i ++) {
+            System.out.print((temp.indexOf(arr[i]) + 1) + " ");
+        }
+    }
 }
